317c28c176f950aa876463aa5ae904d578c96666
Y3K <dboy.y3k@gmail.com>
2014-08-21 12:07:29 -0500

    Layout

--- package.js
+++ package.js
@@ -5,8 +5,8 @@ Package.describe({
 Package.on_use(function (api) {
   api.use(['presence', 'iron-router']);
   api.use(['jquery', 'caret-position'], 'client');
-  api.add_files('jquery.caret.js', 'client');
-  api.add_files('live-caret.js', 'client');
+
+  api.add_files(['lib/jquery.caret.js', 'lib/live-caret.js'], 'client');
 
   api.export('liveCaret', 'client');
 });
